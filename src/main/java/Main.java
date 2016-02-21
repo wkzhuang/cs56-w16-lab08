65110200f27e2cafd66f6bfd11d9a6aa769617b9
wkzhuang <jameszwk9596@gmail.com>
2016-02-21 14:38:46 -0800

    Define config vars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,6 +27,7 @@ public class Main {
 
     get("/hello", (req, res) -> {
 	    RelativisticModel.select();
+	    String energy = System.getenv().get("ENERGY");
 	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
 	    return "E=mc^2: 12 Gev = " + m.toString();
 	});
