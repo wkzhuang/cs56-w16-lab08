5cdff7813c0a5d94a49ce43f663a86ea9db78f8a
wkzhuang <jameszwk9596@gmail.com>
2016-02-21 15:01:00 -0800

    Modify Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,7 @@ public class Main {
 	    RelativisticModel.select();
 	    String energy = System.getenv().get("ENERGY");
 	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-	    return "E=mc^2: 12 Gev = " + m.toString();
+	    return "E=mc^2: " + energy + " = " + m.toString();
 	});
 
     get ("/ucsb", (req,res) -> "Go Gauchos");
